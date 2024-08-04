3e70d3e00e50510c8ddd35cc923fb581a564cefb
ooheunda <ooheunda@gmail.com>
2024-08-05 00:16:37 +0900

    :sparkles: Add posts findAll api

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Post } from 'src/common/entities/post.entity';
+import { Company } from 'src/common/entities/company.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Post, Company])],
   controllers: [PostsController],
   providers: [PostsService],
 })
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,15 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Post } from 'src/common/entities/post.entity';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Company } from 'src/common/entities/company.entity';
+import _ from 'lodash';
 
 @Injectable()
 export class PostsService {
+  constructor(
+    @InjectRepository(Post) private postRepo: Repository<Post>,
+    @InjectRepository(Company) private companyRepo: Repository<Company>,
+  ) {}
+
   create(createPostDto: CreatePostDto) {
     return 'This action adds a new post';
   }
 
-  findAll() {
-    return `This action returns all posts`;
+  async findAll() {
+    // TODO: (qs) pagination, search
+    // TODO: raw query
+    const posts = await this.postRepo
+      .createQueryBuilder('post')
+      .select([
+        'post.id',
+        'post.title',
+        'post.techStack',
+        'post.reward',
+        'post.createdAt',
+      ])
+      .leftJoin('post.company', 'company')
+      .addSelect(['company.name', 'company.region'])
+      .where('post.deletedAt IS NULL')
+      .orderBy('post.createdAt', 'DESC')
+      .getMany();
+
+    if (_.isNil(posts)) throw new NotFoundException();
+
+    return posts.map(({ company, ...v }) => ({
+      ...v,
+      company_name: company.name,
+      region: company.region,
+    }));
   }
 
   findOne(id: number) {
