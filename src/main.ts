efd1ea22afb08302b2eeb0846a03e4d875ea79ba
ooheunda <ooheunda@gmail.com>
2024-08-06 00:18:11 +0900

    :wrench: Set swagger

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Twoted')
+    .setDescription('원티드 프리온보딩 백엔드 인턴십 선발과제')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const PORT = app.get<ConfigService>(ConfigService).get('PORT');
   await app.listen(PORT);
